e3f21482f142dbfe6e5a0a3b0f5fbc6327acaa49
laurentiucalonfir <laurentiucalonfir@gmail.com>
2026-06-24 23:15:26 +0300

    update

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'comenzi-wa-v80';
+const CACHE = 'comenzi-wa-v81';
 const ASSETS = [
   './manifest.json',
   './css/style.css',
   './js/data.js',
   './js/firebase-init.js',
-  './js/sync.js?v=80',
-  './js/app.js?v=80'
+  './js/sync.js?v=81',
+  './js/app.js?v=81'
 ];
 
 self.addEventListener('install', e => {
