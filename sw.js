e3bb2082a1d4ad98aba9a0d507927f01728a190a
laurentiucalonfir <laurentiucalonfir@gmail.com>
2026-06-26 01:21:45 +0300

    update

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'comenzi-wa-v129';
+const CACHE = 'comenzi-wa-v130';
 const ASSETS = [
   './manifest.json',
   './css/style.css',
   './js/data.js',
   './js/firebase-init.js',
-  './js/sync.js?v=129',
-  './js/app.js?v=129'
+  './js/sync.js?v=130',
+  './js/app.js?v=130'
 ];
 
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
